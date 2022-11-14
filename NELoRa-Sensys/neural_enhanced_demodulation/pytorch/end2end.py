3fd9567030f15a0d7c0ad7e2794353e71644cd47
LOLIN-G <hbzhang879@gmail.com>
2022-11-14 11:38:52 -0500

    add

--- NELoRa-Sensys/neural_enhanced_demodulation/pytorch/end2end.py
+++ NELoRa-Sensys/neural_enhanced_demodulation/pytorch/end2end.py
@@ -81,7 +81,7 @@ def load_checkpoint(opts):
     """
 
     maskCNN_path = os.path.join(opts.checkpoint_dir, str(opts.load_iters) + '_maskCNN.pkl')
-    import pdb
+    # import pdb
     
     maskCNN = maskCNNModel(opts)
 
@@ -210,12 +210,217 @@ def training_loop(training_dataloader_X, training_dataloader_Y, testing_dataload
     fixed_Y = to_var(fixed_Y)
     # print("Fixed_X {}".format(fixed_X.shape))
     fixed_X_spectrum_raw = torch.stft(input=fixed_X, n_fft=opts.stft_nfft, hop_length=opts.stft_overlap,
-                                      win_length=opts.stft_window, pad_mode='constant');
+                                      win_length=opts.stft_window, pad_mode='constant')
     fixed_X_spectrum = spec_to_network_input(fixed_X_spectrum_raw, opts)
     # print("Fixed {}".format(fixed_X_spectrum.shape))
 
     fixed_Y_spectrum_raw = torch.stft(input=fixed_Y, n_fft=opts.stft_nfft, hop_length=opts.stft_overlap,
                                       win_length=opts.stft_window, pad_mode='constant');
+    fixed_Y_spectrum = spec_to_network_input(fixed_Y_spectrum_raw, opts)
+
+    iter_per_epoch = min(len(iter_X), len(iter_Y))
+
+    for iteration in range(1, opts.train_iters + 1):
+        if iteration % iter_per_epoch == 0:
+            iter_X = iter(training_dataloader_X)
+            iter_Y = iter(training_dataloader_Y)
+
+        images_X, name_X = iter_X.next()
+        labels_X_mapping = list(
+            map(lambda x: int(x.split('_')[5]), name_X))
+        images_X, labels_X = to_var(images_X), to_var(
+            torch.tensor(labels_X_mapping))
+        images_Y, name_Y = iter_Y.next()
+        labels_Y_mapping = list(
+            map(lambda x: int(x.split('_')[5]), name_Y))
+        images_Y, labels_Y = to_var(images_Y), to_var(
+            torch.tensor(labels_Y_mapping))
+
+        # ============================================
+        #            TRAIN THE GENERATOR
+        # ============================================
+
+        images_X_spectrum_raw = torch.stft(input=images_X, n_fft=opts.stft_nfft, hop_length=opts.stft_overlap,
+
+                                           win_length=opts.stft_window, pad_mode='constant');
+        images_X_spectrum = spec_to_network_input(images_X_spectrum_raw, opts)
+
+        images_Y_spectrum_raw = torch.stft(input=images_Y, n_fft=opts.stft_nfft, hop_length=opts.stft_overlap,
+                                           win_length=opts.stft_window, pad_mode='constant');
+        images_Y_spectrum = spec_to_network_input(images_Y_spectrum_raw, opts)
+        #########################################
+        ##    FILL THIS IN: X--Y               ##
+        #########################################
+        if iteration % 50 == 0:
+            print("Iteration: {}/{}".format(iteration, opts.train_iters))
+        fake_Y_spectrum = mask_CNN(images_X_spectrum)
+        # 2. Compute the generator loss based on domain Y
+        g_y_pix_loss = loss_spec(fake_Y_spectrum, images_Y_spectrum)
+        labels_X_estimated = C_XtoY(fake_Y_spectrum)
+        g_y_class_loss = loss_class(labels_X_estimated, labels_X)
+        g_optimizer.zero_grad()
+        G_Image_loss = opts.scaling_for_imaging_loss * g_y_pix_loss
+        G_Class_loss = opts.scaling_for_classification_loss * g_y_class_loss
+        G_Y_loss = G_Image_loss + G_Class_loss
+        G_Y_loss.backward()
+        g_optimizer.step()
+
+        # Print the log info
+        if iteration % opts.log_step == 0:
+            print(
+                'Iteration [{:5d}/{:5d}] | G_Y_loss: {:6.4f}| G_Image_loss: {:6.4f}| G_Class_loss: {:6.4f}'
+                    .format(iteration, opts.train_iters,
+                            G_Y_loss.item(),
+                            G_Image_loss.item(),
+                            G_Class_loss.item()))
+
+        # Save the generated samples
+        if (iteration % opts.sample_every == 0) and (not opts.server):
+            # save_samples(iteration, fixed_Y_spectrum, fixed_X_spectrum, mask_CNN, opts)
+            save_samples_separate(iteration, fixed_Y_spectrum, fixed_X_spectrum,
+                                  mask_CNN, opts, name_X_fixed, name_Y_fixed, opts.sample_dir)
+
+        # Save the model parameters
+        if iteration % opts.checkpoint_every == 0:
+            checkpoint(iteration, mask_CNN, C_XtoY, opts)
+
+    test_iter_X = iter(testing_dataloader_X)
+    test_iter_Y = iter(testing_dataloader_Y)
+    iter_per_epoch_test = min(len(test_iter_X), len(test_iter_Y))
+
+    error_matrix = np.zeros([len(opts.snr_list), 1], dtype=float)
+    error_matrix_count = np.zeros([len(opts.snr_list), 1], dtype=int)
+
+    error_matrix_info = []
+
+    # iter_per_epoch_test = 500
+    saved_data = {}
+    for iteration in range(iter_per_epoch_test):
+        images_X_test, name_X_test = test_iter_X.next()
+
+        code_X_test_mapping = list(
+            map(lambda x: float(x.split('_')[0]), name_X_test))
+
+        snr_X_test_mapping = list(
+            map(lambda x: int(x.split('_')[1]), name_X_test))
+
+        instance_X_test_mapping = list(
+            map(lambda x: int(x.split('_')[4]), name_X_test))
+
+        labels_X_test_mapping = list(
+            map(lambda x: int(x.split('_')[5]), name_X_test))
+
+        images_X_test, labels_X_test = to_var(images_X_test), to_var(
+            torch.tensor(labels_X_test_mapping))
+
+        images_Y_test, labels_Y_test = test_iter_Y.next()
+        images_Y_test = to_var(images_Y_test)
+
+        images_X_test_spectrum_raw = torch.stft(input=images_X_test, n_fft=opts.stft_nfft,
+                                                hop_length=opts.stft_overlap, win_length=opts.stft_window,
+                                                pad_mode='constant');
+        images_X_test_spectrum = spec_to_network_input(images_X_test_spectrum_raw, opts)
+
+        images_Y_test_spectrum_raw = torch.stft(input=images_Y_test, n_fft=opts.stft_nfft,
+                                                hop_length=opts.stft_overlap, win_length=opts.stft_window,
+                                                pad_mode='constant');
+        images_Y_test_spectrum = spec_to_network_input(images_Y_test_spectrum_raw, opts)
+        fake_Y_test_spectrum = mask_CNN(images_X_test_spectrum)
+        labels_X_estimated = C_XtoY(fake_Y_test_spectrum)
+        saved_sample = to_data(labels_X_estimated)
+
+        for i, label in enumerate(to_data(labels_X_test)):
+            if label not in saved_data.keys():
+                saved_data[label] = []
+                saved_data[label].append(saved_sample[i])
+            else:
+                saved_data[label].append(saved_sample[i])
+        _, labels_X_test_estimated = torch.max(labels_X_estimated, 1)
+
+        test_right_case = (labels_X_test_estimated == labels_X_test)
+        test_right_case = to_data(test_right_case)
+
+        for batch_index in range(opts.batch_size):
+            try:
+                snr_index = opts.snr_list.index(snr_X_test_mapping[batch_index])
+                error_matrix[snr_index] += test_right_case[batch_index]
+                error_matrix_count[snr_index] += 1
+                error_matrix_info.append([instance_X_test_mapping[batch_index], code_X_test_mapping[batch_index],
+                                            snr_X_test_mapping[batch_index],
+                                            labels_X_test_estimated[batch_index].cpu().data.int(),
+                                            labels_X_test[batch_index].cpu().data.int()])
+            except:
+                print("Something else went wrong")
+        if iteration % opts.log_step == 0:
+            print('Testing Iteration [{:5d}/{:5d}]'
+                  .format(iteration, iter_per_epoch_test))
+    error_matrix = np.divide(error_matrix, error_matrix_count)
+    error_matrix_info = np.array(error_matrix_info)
+    scipy.io.savemat(
+        opts.root_path + '/' + opts.dir_comment + '_' + str(opts.sf) + '_' + str(opts.bw) + '.mat',
+        dict(error_matrix=error_matrix,
+             error_matrix_count=error_matrix_count,
+             error_matrix_info=error_matrix_info))
+
+    with open('test.npy', 'wb') as f:
+        np.save(f, saved_data)
+        f.close()
+
+def _TS_train(teacher, student, dataset):
+    distill_criterion = nn.CrossEntropyLoss()
+    clf_criterion = nn.CrossEntropyLoss()
+    teacher.eval()
+    student.train()
+    optimizer = optim.SGD(student.parameters(), lr=1e-4)
+    for data, label in dataset:
+        teacher_logits = teacher(data)
+        student_logits = student(data)
+        distill_loss = distill_criterion(teacher_logits, student_logits)
+        clf_loss = clf_criterion(student_logits, label)
+        loss = distill_loss + clf_loss
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    return student
+        
+def TS_train(training_dataloader_X, training_dataloader_Y, testing_dataloader_X,
+                  testing_dataloader_Y, opts):
+    """Runs the training loop.
+        * Saves checkpoint every opts.checkpoint_every iterations
+        * Saves generated samples every opts.sample_every iterations
+    """
+    loss_spec = torch.nn.MSELoss(reduction='mean')
+    loss_class = nn.CrossEntropyLoss()
+    # Create generators and discriminators
+    if opts.load:
+        mask_CNN, C_XtoY = load_checkpoint(opts)
+    else:
+        mask_CNN, C_XtoY = create_model(opts)
+
+    g_params = list(mask_CNN.parameters()) + list(C_XtoY.parameters())
+    g_optimizer = optim.Adam(g_params, opts.lr, [opts.beta1, opts.beta2])
+
+    iter_X = iter(training_dataloader_X)
+    iter_Y = iter(training_dataloader_Y)
+
+    test_iter_X = iter(testing_dataloader_X)
+    test_iter_Y = iter(testing_dataloader_Y)
+
+    # Get some fixed data from domains X and Y for sampling. These are images that are held
+    # constant throughout training, that allow us to inspect the model's performance.
+    fixed_X, name_X_fixed = test_iter_X.next()
+    fixed_X = to_var(fixed_X)
+
+    fixed_Y, name_Y_fixed = test_iter_Y.next()
+    fixed_Y = to_var(fixed_Y)
+    # print("Fixed_X {}".format(fixed_X.shape))
+    fixed_X_spectrum_raw = torch.stft(input=fixed_X, n_fft=opts.stft_nfft, hop_length=opts.stft_overlap,
+                                      win_length=opts.stft_window, pad_mode='constant')
+    fixed_X_spectrum = spec_to_network_input(fixed_X_spectrum_raw, opts)
+    # print("Fixed {}".format(fixed_X_spectrum.shape))
+
+    fixed_Y_spectrum_raw = torch.stft(input=fixed_Y, n_fft=opts.stft_nfft, hop_length=opts.stft_overlap,
+                                      win_length=opts.stft_window, pad_mode='constant')
     fixed_Y_spectrum = spec_to_network_input(fixed_Y_spectrum_raw, opts)
 
     iter_per_epoch = min(len(iter_X), len(iter_Y))
--- NELoRa-Sensys/neural_enhanced_demodulation/pytorch/models/model_components.py
+++ NELoRa-Sensys/neural_enhanced_demodulation/pytorch/models/model_components.py
@@ -39,7 +39,6 @@ class classificationHybridModel(nn.Module):
         out = self.fcn2(out)
         return out
 
-
 class maskCNNModel(nn.Module):
     def __init__(self, opts):
         super(maskCNNModel, self).__init__()
@@ -85,6 +84,79 @@ class maskCNNModel(nn.Module):
             nn.Conv2d(64, 8, kernel_size=(1, 1), dilation=(1, 1)),
             nn.BatchNorm2d(8), nn.ReLU(),
 
+        )
+
+        self.lstm = nn.LSTM(
+            opts.conv_dim_lstm,
+            opts.lstm_dim,
+            batch_first=True,
+            bidirectional=True)
+
+        self.fc1 = nn.Linear(2 * opts.lstm_dim, opts.fc1_dim)
+        self.fc2 = nn.Linear(opts.fc1_dim, opts.freq_size * opts.y_image_channel)
+
+    def forward(self, x):
+        out = x.transpose(2, 3).contiguous()
+        out = self.conv(out)
+        out = out.transpose(1, 2).contiguous()
+        out = out.view(out.size(0), out.size(1), -1)
+        out, _ = self.lstm(out)
+        out = F.relu(out)
+        out = self.fc1(out)
+        out = F.relu(out)
+        out = self.fc2(out)
+
+        out = out.view(out.size(0), out.size(1), self.opts.y_image_channel, -1)
+        out = torch.sigmoid(out)
+        out = out.transpose(1, 2).contiguous()
+        out = out.transpose(2, 3).contiguous()
+        masked = out * x  # out is mask, masked is denoised
+        return masked
+
+class StudentMaskCNNModel(nn.Module):
+    def __init__(self, opts):
+        super(maskCNNModel, self).__init__()
+        self.opts = opts
+
+        self.conv = nn.Sequential(
+            # cnn1
+            nn.ZeroPad2d((3, 3, 0, 0)),
+            nn.Conv2d(opts.x_image_channel, 64, kernel_size=(1, 7), dilation=(1, 1)),
+            nn.BatchNorm2d(64), nn.ReLU(),
+
+            # cnn2
+            nn.ZeroPad2d((0, 0, 3, 3)),
+            nn.Conv2d(64, 64, kernel_size=(7, 1), dilation=(1, 1)),
+            nn.BatchNorm2d(64), nn.ReLU(),
+
+            # # cnn3
+            # nn.ZeroPad2d(2),
+            # nn.Conv2d(64, 64, kernel_size=(5, 5), dilation=(1, 1)),
+            # nn.BatchNorm2d(64), nn.ReLU(),
+
+            # # cnn4
+            # nn.ZeroPad2d((2, 2, 4, 4)),
+            # nn.Conv2d(64, 64, kernel_size=(5, 5), dilation=(2, 1)),
+            # nn.BatchNorm2d(64), nn.ReLU(),
+
+            # # cnn5
+            # nn.ZeroPad2d((2, 2, 8, 8)),
+            # nn.Conv2d(64, 64, kernel_size=(5, 5), dilation=(4, 1)),
+            # nn.BatchNorm2d(64), nn.ReLU(),
+
+            # # cnn6
+            # nn.ZeroPad2d((2, 2, 16, 16)),
+            # nn.Conv2d(64, 64, kernel_size=(5, 5), dilation=(8, 1)),
+            # nn.BatchNorm2d(64), nn.ReLU(),
+
+            # # cnn7
+            # nn.ZeroPad2d((2, 2, 32, 32)),
+            # nn.Conv2d(64, 64, kernel_size=(5, 5), dilation=(16, 1)),
+            # nn.BatchNorm2d(64), nn.ReLU(),
+
+            # # cnn8
+            # nn.Conv2d(64, 8, kernel_size=(1, 1), dilation=(1, 1)),
+            # nn.BatchNorm2d(8), nn.ReLU(),
         )
 
         self.lstm = nn.LSTM(
